baf63eecd756429c800197bacbcc27e745dcda4b
Kiswono Prayogo <kiswono@gmail.com>
2023-07-04 04:22:17 +0700

    try prometheus-operator

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ terraform {
     }
   }
   backend "local" {
-    path = "/tmp/pf1.tfstate"
+    path = "./pf1.tfstate"
   }
 }
 provider "kubernetes" {
   config_path    = "~/.kube/config"
-  # from k config view | grep -A 3 minikube | grep server:
+  # from: k config view | grep -A 3 minikube | grep server:
   host           = "https://240.1.0.2:8443"
   config_context = "minikube"
 }
@@ -24,6 +24,7 @@ provider "helm" {
   }
 }
 resource "kubernetes_namespace_v1" "pf1ns" {
+  # if already created: terraform import kubernetes_namespace_v1.pf1ns pf1ns
   metadata {
     name        = "pf1ns"
     annotations = {
@@ -76,7 +77,8 @@ resource "kubernetes_service_v1" "pf1svc" {
       app = kubernetes_deployment_v1.promfiberdeploy.spec.0.template.0.metadata.0.labels.app
     }
     port {
-      port        = 33000 # no effect in minikube, will forwarded to random port anyway
+      port        = 3001
+      node_port   = 32000
       target_port = kubernetes_deployment_v1.promfiberdeploy.spec.0.template.0.spec.0.container.0.port.0.container_port
     }
     type = "NodePort"
@@ -109,152 +111,207 @@ resource "kubernetes_ingress_v1" "pf1ingress" {
     }
   }
 }
-resource "kubernetes_config_map_v1" "prom1conf" {
-  metadata {
-    name      = "prom1conf"
-    namespace = kubernetes_namespace_v1.pf1ns.metadata.0.name
-  }
-  data = {
-    # from https://github.com/techiescamp/kubernetes-prometheus/blob/master/config-map.yaml
-    "prometheus.yml" : <<EOF
-global:
-  scrape_interval: 15s
-  evaluation_interval: 15s
-alerting:
-  alertmanagers:
-    - static_configs:
-        - targets:
-          # - alertmanager:9093
-rule_files:
-  #- /etc/prometheus/prometheus.rules
-scrape_configs:
-  - job_name: "prometheus"
-    static_configs:
-      - targets: ["localhost:9090"]
-  - job_name: "pf1"
-    static_configs:
-      - targets: [
-          "${kubernetes_ingress_v1.pf1ingress.spec.0.rule.0.host}:${kubernetes_service_v1.pf1svc.spec.0.port.0.port}"
-        ]
-EOF
-    # need to delete stateful set if this changed after terraform apply
-    # or kubectl rollout restart statefulset prom1stateful -n pf1ns
-    # because statefulset pod not restarted automatically when changed
-    # if configmap set as env or config file
-  }
+## without operator, if there's multiple pod only one getting fetched by prometheus
+#resource "kubernetes_config_map_v1" "prom1conf" {
+#  metadata {
+#    name      = "prom1conf"
+#    namespace = kubernetes_namespace_v1.pf1ns.metadata.0.name
+#  }
+#  data = {
+#    # from https://github.com/techiescamp/kubernetes-prometheus/blob/master/config-map.yaml
+#    "prometheus.yml" : <<EOF
+#global:
+#  scrape_interval: 15s
+#  evaluation_interval: 15s
+#alerting:
+#  alertmanagers:
+#    - static_configs:
+#        - targets:
+#          # - alertmanager:9093
+#rule_files:
+#  #- /etc/prometheus/prometheus.rules
+#scrape_configs:
+#  - job_name: "prometheus"
+#    static_configs:
+#      - targets: ["localhost:9090"]
+#  - job_name: "pf1"
+#    static_configs:
+#      - targets: [
+#          "${kubernetes_ingress_v1.pf1ingress.spec.0.rule.0.host}:${kubernetes_service_v1.pf1svc.spec.0.port.0.port}"
+#        ]
+#EOF
+#    # need to delete stateful set if this changed after terraform apply
+#    # or kubectl rollout restart statefulset prom1stateful -n pf1ns
+#    # because statefulset pod not restarted automatically when changed
+#    # if configmap set as env or config file
+#  }
+#}
+#resource "kubernetes_persistent_volume_v1" "prom1datavol" {
+#  metadata {
+#    name = "prom1datavol"
+#  }
+#  spec {
+#    access_modes = ["ReadWriteOnce"]
+#    capacity     = {
+#      storage = "1Gi"
+#    }
+#    # do not add storage_class_name or it would stuck
+#    persistent_volume_source {
+#      host_path {
+#        path = "/tmp/prom1data" # mkdir first?
+#      }
+#    }
+#  }
+#}
+#resource "kubernetes_persistent_volume_claim_v1" "prom1dataclaim" {
+#  metadata {
+#    name      = "prom1dataclaim"
+#    namespace = kubernetes_namespace_v1.pf1ns.metadata.0.name
+#  }
+#  spec {
+#    # do not add storage_class_name or it would stuck
+#    access_modes = ["ReadWriteOnce"]
+#    resources {
+#      requests = {
+#        storage = "1Gi"
+#      }
+#    }
+#  }
+#}
+#resource "kubernetes_stateful_set_v1" "prom1stateful" {
+#  metadata {
+#    name      = "prom1stateful"
+#    namespace = kubernetes_namespace_v1.pf1ns.metadata.0.name
+#    labels    = {
+#      app = "prom1"
+#    }
+#  }
+#  spec {
+#    selector {
+#      match_labels = {
+#        app = "prom1"
+#      }
+#    }
+#    template {
+#      metadata {
+#        labels = {
+#          app = "prom1"
+#        }
+#      }
+#      # example: https://github.com/mateothegreat/terraform-kubernetes-monitoring-prometheus/blob/main/deployment.tf
+#      spec {
+#        container {
+#          name  = "prometheus"
+#          image = "prom/prometheus:latest"
+#          args  = [
+#            "--config.file=/etc/prometheus/prometheus.yml",
+#            "--storage.tsdb.path=/prometheus/",
+#            "--web.console.libraries=/etc/prometheus/console_libraries",
+#            "--web.console.templates=/etc/prometheus/consoles",
+#            "--web.enable-lifecycle",
+#            "--web.enable-admin-api",
+#            "--web.listen-address=:10902"
+#          ]
+#          port {
+#            name           = "http1"
+#            container_port = 10902
+#          }
+#          volume_mount {
+#            name       = kubernetes_config_map_v1.prom1conf.metadata.0.name
+#            mount_path = "/etc/prometheus/"
+#          }
+#          volume_mount {
+#            name       = "prom1datastorage"
+#            mount_path = "/prometheus/"
+#          }
+#          #security_context {
+#          #  run_as_group = "1000" # because /tmp/prom1data is owned by 1000
+#          #}
+#        }
+#        volume {
+#          name = kubernetes_config_map_v1.prom1conf.metadata.0.name
+#          config_map {
+#            default_mode = "0666"
+#            name         = kubernetes_config_map_v1.prom1conf.metadata.0.name
+#          }
+#        }
+#        volume {
+#          name = "prom1datastorage"
+#          persistent_volume_claim {
+#            claim_name = kubernetes_persistent_volume_claim_v1.prom1dataclaim.metadata.0.name
+#          }
+#        }
+#      }
+#    }
+#    service_name = ""
+#  }
+#}
+#resource "kubernetes_service_v1" "prom1svc" {
+#  metadata {
+#    name      = "prom1svc"
+#    namespace = kubernetes_namespace_v1.pf1ns.metadata.0.name
+#  }
+#  spec {
+#    selector = {
+#      app = kubernetes_stateful_set_v1.prom1stateful.spec.0.template.0.metadata.0.labels.app
+#    }
+#    port {
+#      port        = 10902 # no effect in minikube, will forwarded to random port anyway
+#      target_port = kubernetes_stateful_set_v1.prom1stateful.spec.0.template.0.spec.0.container.0.port.0.container_port
+#    }
+#    type = "NodePort"
+#  }
+#}
+# operator version, solves multiple instance of pod issue
+# manually:
+# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+# helm search repo prometheus-community
+# helm install pf1prom prometheus-community/kube-prometheus-stack -n pf1ns
+resource "helm_release" "pf1prom" {
+  name       = "pf1prom"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  namespace  = kubernetes_namespace_v1.pf1ns.metadata.0.name
+  # uninstall: https://github.com/prometheus-community/helm-charts/tree/main/charts/kube-prometheus-stack#kube-prometheus-stack
 }
-resource "kubernetes_persistent_volume_v1" "prom1datavol" {
-  metadata {
-    name = "prom1datavol"
-  }
-  spec {
-    access_modes = ["ReadWriteOnce"]
-    capacity     = {
-      storage = "1Gi"
-    }
-    # do not add storage_class_name or it would stuck
-    persistent_volume_source {
-      host_path {
-        path = "/tmp/prom1data" # mkdir first?
+resource "kubernetes_manifest" "pf1prompodmonitor" {
+  manifest = {
+    "apiVersion" = "monitoring.coreos.com/v1"
+    "kind"       = "PodMonitor"
+    "metadata"   = {
+      "name"      = "pf1prompodmonitor"
+      "namespace" = kubernetes_namespace_v1.pf1ns.metadata.0.name
+      "labels"    = {
+        "name" = "pf1podmonitor"
       }
     }
-  }
-}
-resource "kubernetes_persistent_volume_claim_v1" "prom1dataclaim" {
-  metadata {
-    name      = "prom1dataclaim"
-    namespace = kubernetes_namespace_v1.pf1ns.metadata.0.name
-  }
-  spec {
-    # do not add storage_class_name or it would stuck
-    access_modes = ["ReadWriteOnce"]
-    resources {
-      requests = {
-        storage = "1Gi"
-      }
-    }
-  }
-}
-resource "kubernetes_stateful_set_v1" "prom1stateful" {
-  metadata {
-    name      = "prom1stateful"
-    namespace = kubernetes_namespace_v1.pf1ns.metadata.0.name
-    labels    = {
-      app = "prom1"
-    }
-  }
-  spec {
-    selector {
-      match_labels = {
-        app = "prom1"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          app = "prom1"
+    "spec" = {
+      "selector" = {
+        "matchLabels" = {
+          "app" = kubernetes_deployment_v1.promfiberdeploy.spec.0.selector.0.match_labels.app
         }
       }
-      # example: https://github.com/mateothegreat/terraform-kubernetes-monitoring-prometheus/blob/main/deployment.tf
-      spec {
-        container {
-          name  = "prometheus"
-          image = "prom/prometheus:latest"
-          args  = [
-            "--config.file=/etc/prometheus/prometheus.yml",
-            "--storage.tsdb.path=/prometheus/",
-            "--web.console.libraries=/etc/prometheus/console_libraries",
-            "--web.console.templates=/etc/prometheus/consoles",
-            "--web.enable-lifecycle",
-            "--web.enable-admin-api",
-            "--web.listen-address=:10902"
-          ]
-          port {
-            name           = "http1"
-            container_port = 10902
-          }
-          volume_mount {
-            name       = kubernetes_config_map_v1.prom1conf.metadata.0.name
-            mount_path = "/etc/prometheus/"
-          }
-          volume_mount {
-            name       = "prom1datastorage"
-            mount_path = "/prometheus/"
-          }
-          #security_context {
-          #  run_as_group = "1000" # because /tmp/prom1data is owned by 1000
-          #}
+      "podMetricsEndpoints" = [
+        {
+          "port" = kubernetes_deployment_v1.promfiberdeploy.spec.0.template.0.spec.0.container.0.port.0.container_port
         }
-        volume {
-          name = kubernetes_config_map_v1.prom1conf.metadata.0.name
-          config_map {
-            default_mode = "0666"
-            name         = kubernetes_config_map_v1.prom1conf.metadata.0.name
-          }
-        }
-        volume {
-          name = "prom1datastorage"
-          persistent_volume_claim {
-            claim_name = kubernetes_persistent_volume_claim_v1.prom1dataclaim.metadata.0.name
-          }
-        }
-      }
+      ]
     }
-    service_name = ""
   }
 }
-resource "kubernetes_service_v1" "prom1svc" {
+resource "kubernetes_service_v1" "pf1promsvc" {
   metadata {
-    name      = "prom1svc"
+    name      = "pf1promsvc"
     namespace = kubernetes_namespace_v1.pf1ns.metadata.0.name
   }
   spec {
     selector = {
-      app = kubernetes_stateful_set_v1.prom1stateful.spec.0.template.0.metadata.0.labels.app
+      "app.kubernetes.io/name" = "prometheus" # default for prometheus operator
     }
     port {
-      port        = 10902 # no effect in minikube, will forwarded to random port anyway
-      target_port = kubernetes_stateful_set_v1.prom1stateful.spec.0.template.0.spec.0.container.0.port.0.container_port
+      target_port = 9090 # default port for prometheus-pf1prom-kube-prometheus-st-prometheus-0 pod
+      port        = 9091
+      node_port   = 30900
     }
     type = "NodePort"
   }
@@ -267,13 +324,15 @@ resource "helm_release" "pf1keda" {
   # uninstall: https://keda.sh/docs/2.11/deploy/#helm
 }
 # run with this commented first, then uncomment
+# or helm install manually pf1keda kedacore/keda
+# since by default scaledobject not there in: kubectl api-resources
 # from: https://www.youtube.com/watch?v=1kEKrhYMf_g
-resource "kubernetes_manifest" "scaled_object" {
+resource "kubernetes_manifest" "pf1kedascaledobject" {
   manifest = {
-    "apiVersion" = "keda.sh/v1alpha1"
+    "apiVersion" = "keda.sh/v1alpha1" # error "cannot select exact GV from REST mapper" if wrong
     "kind"       = "ScaledObject"
     "metadata"   = {
-      "name"      = "pf1scaledobject"
+      "name"      = "pf1kedascaledobject"
       "namespace" = kubernetes_namespace_v1.pf1ns.metadata.0.name
     }
     "spec" = {
