7d3319482989b50ec9963171f4972089504c5a2b
Kiswono Prayogo <kiswono@gmail.com>
2026-07-09 10:33:00 +0800

    Remove terraform1 ximage dependency

--- scripts/update-vulnerable-dependencies.sh
+++ scripts/update-vulnerable-dependencies.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 GOFLAGS=-mod=mod go get golang.org/x/crypto@v0.53.0
-GOFLAGS=-mod=mod go get golang.org/x/image@v0.18.0
+go mod edit -droprequire=golang.org/x/image
 go mod edit -exclude=golang.org/x/crypto@v0.14.0
+go mod edit -exclude=golang.org/x/image@v0.0.0-20190227222117-0694c2d4d067
+go mod edit -exclude=golang.org/x/image@v0.0.0-20190802002840-cff245a6509b
 GOFLAGS=-mod=mod go mod tidy
 go mod edit -require=golang.org/x/crypto@v0.53.0
-go mod edit -require=golang.org/x/image@v0.18.0
 GOFLAGS=-mod=mod go mod download golang.org/x/crypto
-GOFLAGS=-mod=mod go mod download golang.org/x/image
